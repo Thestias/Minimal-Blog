53b943d3f8c8d57a8bb22810b80bfacf9acdc979
Thestias <franceschiconrado@gmail.com>
2020-07-19 15:52:34 -0300

    Initial Commit

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_blog_website.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'personal_blog_website.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
