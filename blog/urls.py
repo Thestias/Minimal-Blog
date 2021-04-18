1f07fa080c56fbfa54987e3190cbc3032464c7a8
Thestias <franceschiconrado@gmail.com>
2021-04-18 17:15:21 -0300

    Added initial login/register system

--- blog/urls.py
+++ blog/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='Home'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login_register.html',
+                                                redirect_authenticated_user=True), name='Login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='Logout'),
+
+    path('register/', views.register, name='Register')
+    # path('profile/', views.profile, name='profile'),
+
+    # # Password Reset Views
+    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html',
+    #                                                              form_class=CustomPasswordResetForm), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+    #     template_name='users/password_reset_done.html'), name='password_reset_done'),
+
+    # path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    #     template_name='users/password_reset_confirm.html', form_class=CustomSetPasswordForm), name='password_reset_confirm'),
+
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(
+    #     template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreation
+from django.contrib import messages
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
+
+    if request.method == 'POST':
+        register_form = CustomUserCreation(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            username = register_form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('Login')
+    else:
+        register_form = CustomUserCreation()
+    return render(request, 'login_register.html', context={'form': register_form})
